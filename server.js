f2ca584ca1c952d64caa0d5ddc62c0c5d878e64c
AlexT <timis.alexioan@gmail.com>
2024-07-07 00:36:00 +0300

    added insert and find

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const uri = "mongodb+srv://aitimis:Angular!2345.@cluster0.qzzyqmo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
+app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 
 MongoClient.connect(uri, {
@@ -15,27 +16,42 @@ MongoClient.connect(uri, {
   }
 })
 .then(client => {
-  console.log("Connected to Database");
-  const db = client.db('star-wars-quotes');
+    console.log("Connected to Database");
+
+    const db = client.db('star-wars-quotes');
+    
+    const quotesCollection = db.collection('quotes');
 
     // express request handlers
-    // we handle a GET request with the get method
-    // app.get(endpoint, callback);
     app.get('/', (req, res) => {
-        res.sendFile(__dirname + '/index.html')
+        res.sendFile(__dirname + '/index.html');
+
+        const cursor = db.collection('quotes')
+        .find()
+        .toArray()
+        .then(result => {
+            console.log(result)
+        })
+        .catch(error => console.error(error))
     });
 
     app.post('/quotes', (req, res) => {
-        console.log(req.body)
+        // console.log(req.body)
+        quotesCollection
+        .insertOne(req.body)
+        .then(result => { 
+            console.log(result)
+            res.redirect('/')
+        })
+        .catch(error => console.error(error));
     })
 
     // start the server
     app.listen(3000, () => {
         console.log('listening on 3000');
     })
-    
-})
-.catch(error => console.error(error));
+
+}).catch(error => console.error(error));
 
   
 
